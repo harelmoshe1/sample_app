454e1389e73bb00758f19a479875383c520f1dc5
Harel Moshe <harel.m@fiverr.com>
2015-12-28 16:46:31 +0200

    Before 9.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,11 +39,14 @@ end
 private
 
   def signed_in_user
-    redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
   end
 
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
-   end
+  end
 end
