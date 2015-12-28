79230439b2d751637c715b059c70fdcb9b5f2781
Harel Moshe <harel.m@fiverr.com>
2015-12-28 14:24:00 +0200

    Back on track

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,6 @@
 module SessionsHelper
 
- def sign_in(user)
+   def sign_in(user)
     cookies.permanent[:remember_token] = user.remember_token
     self.current_user = user
   end
@@ -9,16 +9,16 @@ module SessionsHelper
     !current_user.nil?
   end
 
+ def current_user=(user)
+    @current_user = user
+  end
+
   def current_user
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
 
-  def current_user=(user)
-    @current_user = user
-  end  
-
   def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)
-  end  
+  end
 end
