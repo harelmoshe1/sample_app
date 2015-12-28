8d0000dc149b2cc2adc71455fce244f6c7339d80
Harel Moshe <harel.m@fiverr.com>
2015-12-28 15:12:25 +0200

    Before 9.2.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 
 class UsersController < ApplicationController
 
+  before_filter :signed_in_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
   end
@@ -9,7 +11,7 @@ class UsersController < ApplicationController
   	@user = User.new
   end
 
-def create
+ def create
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
@@ -18,5 +20,27 @@ def create
     else
       render 'new'
     end
+ end
+
+def edit
+   @user = User.find(params[:id])
+end
+
+def update
+  @user = User.find(params[:id])
+  if @user.update_attributes(params[:user])
+    flash[:success] = "Profile updated"
+    sign_in @user
+    redirect_to @user
+  else
+    render 'edit'
   end
+end
+
+private
+
+  def signed_in_user
+    redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  end
+
 end
