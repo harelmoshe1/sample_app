d2466c53d3af445d15b20b9d2edf482d18305e38
Harel Moshe <harel.m@fiverr.com>
2015-12-29 15:31:50 +0200

    Before 10.3.4

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,6 +20,7 @@ describe User do
   it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
   it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }  
 
   it { should be_valid }
   it { should_not be_admin }
@@ -109,12 +110,24 @@ describe User do
   end
 
   describe "micropost associations" do
+    
     before { @user.save }
+    
     let!(:older_micropost) do
       FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
     end
     let!(:newer_micropost) do
       FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
     end
 
     it "should destroy associated microposts" do
