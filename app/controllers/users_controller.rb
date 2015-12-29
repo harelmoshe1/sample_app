d276ae146208d26fcae99d39ea368ef9eb1ab2d1
Harel Moshe <harel.m@fiverr.com>
2015-12-29 14:22:43 +0200

    Before 10.2.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,3 @@
-
 class UsersController < ApplicationController
 
   before_filter :signed_in_user,      only: [:index, :edit, :update, :destroy]
@@ -8,6 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])    
   end
 
   def index
